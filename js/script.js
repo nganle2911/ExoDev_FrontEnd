62c0902d47ac9a4e1329d2ea8ce234e03dc3ef12
Ngan LE <ngan.ltkn2911@gmail.com>
2026-02-12 11:31:06 +0100

    add dynamic title for each project

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ let projets = [];
 // Cette fonction permet de charger les données des projets depuis le fichier json 
 async function loadData() {
     let response = await fetch("../data/projets.json");
-    // console.log(response);
 
     // Afficher l'erreur si le fichier json n'est pas chargé 
     if (response.ok != true) {
@@ -17,9 +16,10 @@ async function loadData() {
 function renderProjects(projets) {
     let contentHTML = "";
 
-    projets.map((projet, indice) => {
+    // Boucler sur les projets pour afficher les cartes
+    projets.map((projet) => {
         contentHTML += `
-            <div class="projet__carte">
+            <div class="projet__carte" data-title="${projet.titre}">
                 <img class="carte__img" src="./img/${projet.img}" alt="thumbnail-projet" />
                 <img class="carte__mask" src="./img/card-outline.png" alt="outline" aria-hidden="true" />
                 <div class="carte__titre">
@@ -30,6 +30,13 @@ function renderProjects(projets) {
     });
 
     document.querySelector(".accueil__projets").innerHTML = contentHTML;
+
+    // Ajouter un effet de survol sur chaque carte pour que le titre dynamique se change en fonction du titre du projet
+    document.querySelectorAll(".projet__carte").forEach(carte => {
+        carte.addEventListener("mouseover", () => {
+            document.querySelector(".titreDynamique").textContent = carte.dataset.title;
+        });
+    });
 }
 
 async function main() {
